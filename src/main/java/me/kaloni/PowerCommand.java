382bf18fd1ebdc71a8a040ef00a7b9b54491982d
igetpaid044-collab <igetpaid044@gmail.com>
2026-02-19 18:39:10 -0500

    Update PowerCommand.java

--- src/main/java/me/kaloni/PowerCommand.java
+++ src/main/java/me/kaloni/PowerCommand.java
@@ -1,1 +1,18 @@
-Place PNG icons here for each ability with CustomModelData IDs.
+package me.kaloni;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class PowerCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            player.sendMessage("Opening Element Menu...");
+            return true;
+        }
+        return false;
+    }
+}
