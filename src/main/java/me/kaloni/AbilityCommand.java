b23ebcb63b9567de85120bcbb1dabb31bc6868b0
igetpaid044-collab <igetpaid044@gmail.com>
2026-02-19 23:50:38 -0500

    Update AbilityCommand.java

--- src/main/java/me/kaloni/AbilityCommand.java
+++ src/main/java/me/kaloni/AbilityCommand.java
@@ -1,16 +1,11 @@
-package me.kaloni;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-public class AbilityCommand implements CommandExecutor {
+class AbilityCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) return true;
-        Player p = (Player) sender;
-        p.sendMessage("§aAbility system active!");
+    public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
+        if (!(s instanceof Player)) return true;
+        // This checks if you typed /ability 1 or /ability 2
+        int num = (a.length > 0 && a[0].equals("2")) ? 2 : 1;
+        // This triggers the damage, particles, and cooldowns
+        ElementSMP.executeAbility((Player) s, num);
         return true;
     }
 }
