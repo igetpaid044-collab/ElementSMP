ed4ab7fddab3cff4dbbeafdc4b47cb1179dda600
igetpaid044-collab <igetpaid044@gmail.com>
2026-02-19 20:44:39 -0500

    Update ElementSMP.java

--- src/main/java/me/kaloni/ElementSMP.java
+++ src/main/java/me/kaloni/ElementSMP.java
@@ -1,17 +1,41 @@
 package me.kaloni;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.Random;
 
-public class ElementSMP extends JavaPlugin {
+public class ElementSMP extends JavaPlugin implements Listener {
+
+    // The 12 Elements
+    private final String[] elements = {
+        "§cFire", "§bWater", "§aEarth", "§fAir", 
+        "§3Ice", "§2Nature", "§eLightning", "§8Shadow", 
+        "§eLight", "§6Magma", "§5Void", "§7Wind"
+    };
+
     @Override
     public void onEnable() {
-        getLogger().info("ElementSMP has been enabled!");
-        getCommand("elements").setExecutor(new PowerCommand());
+        Bukkit.getPluginManager().registerEvents(this, this);
+        if (getCommand("elements") != null) {
+            getCommand("elements").setExecutor(new PowerCommand());
+        }
     }
 
-    @Override
-    public void onDisable() {
-        getLogger().info("ElementSMP has been disabled!");
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        // Pick a random element from the list
+        String randomElement = elements[new Random().nextInt(elements.length)];
+        
+        // Send the message to the player
+        event.getPlayer().sendMessage("§8§m-------------------------------");
+        event.getPlayer().sendMessage("§6§lElementSMP §7> Welcome!");
+        event.getPlayer().sendMessage("§7You have been assigned the element: " + randomElement);
+        event.getPlayer().sendMessage("§8§m-------------------------------");
+        
+        // Note: In a full plugin, you would save this to a database or file 
+        // so they don't get a new one every single time they join.
     }
 }
