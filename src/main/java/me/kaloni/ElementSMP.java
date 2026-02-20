ebe9f9660dd9d0ccf1fb31dd7250595b414186ed
igetpaid044-collab <igetpaid044@gmail.com>
2026-02-19 20:46:12 -0500

    Update ElementSMP.java

--- src/main/java/me/kaloni/ElementSMP.java
+++ src/main/java/me/kaloni/ElementSMP.java
@@ -1,41 +1,91 @@
 package me.kaloni;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.HashMap;
 import java.util.Random;
+import java.util.UUID;
 
 public class ElementSMP extends JavaPlugin implements Listener {
 
-    // The 12 Elements
+    // This stores which player has which element
+    public static HashMap<UUID, String> playerElements = new HashMap<>();
+
     private final String[] elements = {
-        "§cFire", "§bWater", "§aEarth", "§fAir", 
-        "§3Ice", "§2Nature", "§eLightning", "§8Shadow", 
-        "§eLight", "§6Magma", "§5Void", "§7Wind"
+        "Fire", "Water", "Earth", "Air", 
+        "Ice", "Nature", "Lightning", "Shadow", 
+        "Light", "Magma", "Void", "Wind"
     };
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        if (getCommand("elements") != null) {
-            getCommand("elements").setExecutor(new PowerCommand());
-        }
+        getCommand("elements").setExecutor(new PowerCommand());
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        // Pick a random element from the list
-        String randomElement = elements[new Random().nextInt(elements.length)];
-        
-        // Send the message to the player
-        event.getPlayer().sendMessage("§8§m-------------------------------");
-        event.getPlayer().sendMessage("§6§lElementSMP §7> Welcome!");
-        event.getPlayer().sendMessage("§7You have been assigned the element: " + randomElement);
-        event.getPlayer().sendMessage("§8§m-------------------------------");
-        
-        // Note: In a full plugin, you would save this to a database or file 
-        // so they don't get a new one every single time they join.
+        Player player = event.getPlayer();
+        // Only assign if they don't have one yet
+        if (!playerElements.containsKey(player.getUniqueId())) {
+            String randomElement = elements[new Random().nextInt(elements.length)];
+            playerElements.put(player.getUniqueId(), randomElement);
+            
+            player.sendMessage("§6§lELEMENT §7> You are now a master of: §e" + randomElement);
+        }
     }
-}
+
+    // --- ELEMENT ABILITIES ---
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            String element = playerElements.get(player.getUniqueId());
+
+            if (element == null) return;
+
+            // FIRE/MAGMA: Immune to Fire and Lava damage
+            if ((element.equals("Fire") || element.equals("Magma")) && 
+                (event.getCause() == EntityDamageEvent.DamageCause.FIRE || 
+                 event.getCause() == EntityDamageEvent.DamageCause.LAVA)) {
+                event.setCancelled(true);
+            }
+            
+            // VOID: No Fall Damage
+            if (element.equals("Void") && event.getCause() == EntityDamageEvent.DamageCause.FALL) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        String element = playerElements.get(player.getUniqueId());
+
+        if (element == null) return;
+
+        // WIND: Permanent Speed 2
+        if (element.equals("Wind")) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 40, 1, false, false));
+        }
+
+        // WATER: Permanent Water Breathing
+        if (element.equals("Water")) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 40, 0, false, false));
+        }
+        
+        // ICE: Walk on Water (Frost Walker effect)
+        if (element.equals("Ice") && player.getLocation().getBlock().getType() == Material.WATER) {
+             player.addPotionEffect(new PotionEffect(Potion
